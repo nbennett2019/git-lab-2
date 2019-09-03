e9bf4b2746268211f79b7786e8404347bcef8774
Nathan Bennett <nb566818@ohio.edu>
2019-09-03 17:04:57 -0400

    Initial Commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Nathan Bennett
+ *        Date: September 3, 2019
+ * Description: Displays "Hello Git!!"
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
